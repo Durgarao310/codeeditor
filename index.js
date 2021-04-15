e535aeb9267c5cef7e453bb419c688a7da481583
Durgarao310 <ganta.durgarao310@gmail.com>
2021-04-15 16:32:25 +0530

    d

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const querystring = require('querystring')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const RUN_URL = 'https://api.hackerearth.com/v3/code/run/'
 const cors = require('cors')
 
